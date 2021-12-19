791776923ecce5080821a94428ed4da484d7fa23
Andrey D <turdus@merul.space>
2021-12-20 00:25:01 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+
+API_KEY = "c07d90f95de0fa31f69f050b18f0261f"
+
+
+def get_min_temp_diff():
+    return ""
+
+
+def get_max_day_len():
+    return ""
+
+
+if __name__ == "__main__":
+    print(f"1. {__name__}\n2. {__name__}")
